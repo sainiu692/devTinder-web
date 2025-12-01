eddb218a46eacaa7edb4f0305b11208a21e0ae8a
sainiu692 <sainiu692@gmail.com>
2025-12-01 17:08:09 +0530

    modify url

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:3000";
-export const BASE_URL = "https://dev-tinder-uu3f.onrender.com";
+export const BASE_URL = "https://dev-tinder-backend-kl71.onrender.com";
 
 // export const BASE_URL = "/api"; --> for aws
