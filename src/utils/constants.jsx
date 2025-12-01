c3927e390379aa1935951230adb04ec276102320
sainiu692 <sainiu692@gmail.com>
2025-12-01 16:17:44 +0530

    modify the BASE_URL

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,1 +1,4 @@
-export const BASE_URL = "/api";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://dev-tinder-uu3f.onrender.com";
+
+// export const BASE_URL = "/api"; --> for aws
